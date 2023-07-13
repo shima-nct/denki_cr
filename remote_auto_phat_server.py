10d749b7193e331d5c053082135d490be9ac9b97
SHIMA Naoki <shima@numazu-ct.ac.jp>
2023-07-13 01:21:28 +0000

    Update 冗長なエラー処理を削除
    コメントを追加

--- remote_auto_phat_server.py
+++ remote_auto_phat_server.py
@@ -9,35 +9,43 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--port", type=int, default=3354)
 args = parser.parse_args()
 
+# モータードライバのインスタンス生成
 myMotor = qwiic_scmd.QwiicScmd()
 
+
 def stop_all_motors():
-    # (MOTOR, DIR, SPEED)
-    myMotor.set_drive(0, 0, 0)
+    """
+    モータードライバをまとめて止める．
+    """
+    myMotor.set_drive(0, 0, 0)  # set_drive()の引数はそれぞれ
+                                # MOTOR, DIR, SPEED
     myMotor.set_drive(1, 0, 0)
 
-stop_all_motors()
 
-timeout_time = 300  # [s]
+stop_all_motors()  # 念のために止めておく．
+
+timeout_time = 300  # 単位[s]．何も送られて来なければ通信障害を
+# 想定してモーターを止める．
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serversocket:
     serversocket.bind((socket.gethostname(), args.port))
-    serversocket.listen()
+    serversocket.listen()   # サーバーとして接続要求の受付開始．
 
-    try:
-        while True:
-            (clientsocket, address) = serversocket.accept()
-            clientsocket.settimeout(timeout_time)
-            try:
-                while True:
-                    json_data = clientsocket.recv(1024)
-                    if not json_data:
-                        break
-                    clientsocket.sendall(b'R')
-                    data = json.loads(json_data.decode("utf-8"))
-                    myMotor.set_drive(data["motor"], data["dir"], data["speed"])
-            except socket.timeout:
-                stop_all_motors()
-                clientsocket.close()
-    finally:
-        stop_all_motors()
+    while True:
+        # 接続要求を受け入れる．
+        (clientsocket, address) = serversocket.accept()
+        clientsocket.settimeout(timeout_time)  # 受信ソケットにtimeoutを設定
+        try:
+            while True:
+                # 受信データを取り出す．1024はバッファーサイズ．
+                json_data = clientsocket.recv(1024)
+                clientsocket.sendall(b'R')  # 受領確認として`R`を送る．
+                # 受信データはjson形式で表現されている．
+                # このデータを連想配列に変換する．
+                data = json.loads(json_data.decode("utf-8"))
+                # 連想配列のデータ内の各パラメータを取り出してモータードライバに
+                # 設定する．
+                myMotor.set_drive(data["motor"], data["dir"], data["speed"])
+        except:
+            stop_all_motors()
+            clientsocket.close()

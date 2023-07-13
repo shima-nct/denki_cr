0cdd20e0e3c5d96fcde1f3aff9fd66128228942b
SHIMA Naoki <shima@numazu-ct.ac.jp>
2023-07-13 16:08:00 +0000

    Update 例外処理の整理

--- remote_auto_phat_client.py
+++ remote_auto_phat_client.py
@@ -1,40 +1,56 @@
 import socket
 import json
 import time
+import sys
 import argparse
+
 
 # コマンドラインオプションの処理
 parser = argparse.ArgumentParser()
-parser.add_argument("--remote-host", type=str, default=socket.gethostname())
+parser.add_argument("--remote-host", type=str, default='0.0.0.0')
 parser.add_argument("--remote-port", type=int, default=3354)
 args = parser.parse_args()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((args.remote_host, args.remote_port))
+buffer_size = 1024
 
-    def send_control(control):
-        json_data = json.dumps(control).encode('utf-8')
-        s.sendall(json_data)
-        return_code = ''
-        while True:
-            return_code = s.recv(1024)
-            if return_code == b'R':
-                break
-        return return_code
 
+def send_control(stream, control):
+    json_data = json.dumps(control).encode('utf-8')
+    stream.sendall(json_data)
+    return_code = ''
+    while True:
+        return_code = stream.recv(buffer_size)
+        if return_code == b'R':
+            break
+    return return_code
+
+
+def send_two_motor(_stream, _speed):
+    for motor in range(0, 2):
+        control = {"motor": motor, "dir": 0, "speed": _speed}
+        send_control(_stream, control)
+        control = {"motor": motor, "dir": 0, "speed": _speed}
+        send_control(_stream, control)
+
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as stream:
+        stream.connect((args.remote_host, args.remote_port))
+
+        try:
+            while True:
+                for speed in range(20, 255):
+                    send_two_motor(stream, speed)
+                    time.sleep(0.01)
+                for speed in range(254, 20, -1):
+                    send_two_motor(stream, speed)
+                    time.sleep(0.01)
+        except:
+            send_two_motor(stream, 0)
+
+
+if __name__ == '__main__':
     try:
-        while True:
-            for speed in range(20, 255):
-                control = {"motor": 0, "dir": 0, "speed": speed}
-                return_code = send_control(control)
-                time.sleep(0.01)
-            for speed in range(254, 20, -1):
-                control = {"motor": 0, "dir": 0, "speed": speed}
-                return_code = send_control(control)
-                time.sleep(0.01)
-    finally:
-        control = {"motor": 0, "dir": 0, "speed": 0}
-        return_code = send_control(control)
-        control = {"motor": 1, "dir": 0, "speed": 0}
-        return_code = send_control(control)
-
+        main()
+    except (KeyboardInterrupt, SystemExit) as e:
+        sys.exit(0)
--- remote_auto_phat_server.py
+++ remote_auto_phat_server.py
@@ -1,16 +1,16 @@
 
+import time
 import socket
 import json
 import qwiic_scmd
 import argparse
+import sys
 
-# コマンドラインオプションの処理
-parser = argparse.ArgumentParser()
-parser.add_argument("--port", type=int, default=3354)
-args = parser.parse_args()
 
 # モータードライバのインスタンス生成
 myMotor = qwiic_scmd.QwiicScmd()
+if myMotor.connected == False:
+    raise Exception("Motor Driver not connected. Check connections.")
 
 
 def stop_all_motors():
@@ -18,34 +18,57 @@ def stop_all_motors():
     モータードライバをまとめて止める．
     """
     myMotor.set_drive(0, 0, 0)  # set_drive()の引数はそれぞれ
-                                # MOTOR, DIR, SPEED
+    # MOTOR, DIR, SPEED
     myMotor.set_drive(1, 0, 0)
 
 
+myMotor.begin()
+print("Motor initialized.")
+time.sleep(.250)
 stop_all_motors()  # 念のために止めておく．
+myMotor.enable()
+print("Motor enabled")
+time.sleep(.250)
 
-timeout_time = 300  # 単位[s]．何も送られて来なければ通信障害を
-# 想定してモーターを止める．
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serversocket:
-    serversocket.bind((socket.gethostname(), args.port))
-    serversocket.listen()   # サーバーとして接続要求の受付開始．
+# コマンドラインオプションの処理
+parser = argparse.ArgumentParser()
+parser.add_argument("--port", type=int, default=3354)
+args = parser.parse_args()
 
-    while True:
-        # 接続要求を受け入れる．
-        (clientsocket, address) = serversocket.accept()
-        clientsocket.settimeout(timeout_time)  # 受信ソケットにtimeoutを設定
-        try:
-            while True:
-                # 受信データを取り出す．1024はバッファーサイズ．
-                json_data = clientsocket.recv(1024)
-                clientsocket.sendall(b'R')  # 受領確認として`R`を送る．
-                # 受信データはjson形式で表現されている．
-                # このデータを連想配列に変換する．
-                data = json.loads(json_data.decode("utf-8"))
-                # 連想配列のデータ内の各パラメータを取り出してモータードライバに
-                # 設定する．
-                myMotor.set_drive(data["motor"], data["dir"], data["speed"])
-        except:
-            stop_all_motors()
-            clientsocket.close()
+# 単位[s]．何も送られて来なければ通信障害を想定してモーターを止める．
+timeout_time = 300  
+buffer_size = 1024
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serversocket:
+        serversocket.bind(('0.0.0.0', args.port))
+        serversocket.listen()   # サーバーとして接続要求の受付開始．
+
+        while True:
+            # 接続要求を受け入れる．
+            (clientsocket, address) = serversocket.accept()
+            clientsocket.settimeout(timeout_time)  # 受信ソケットにtimeoutを設定
+            try:
+                while True:
+                    # 受信データを取り出す．
+                    json_data = clientsocket.recv(buffer_size)
+                    clientsocket.sendall(b'R')  # 受領確認として`R`を送る．
+                    # 受信データはjson形式で表現されている．
+                    # このデータを連想配列に変換する．
+                    data = json.loads(json_data.decode("utf-8"))
+                    # 連想配列のデータ内の各パラメータを取り出してモータードライバに
+                    # 設定する．
+                    myMotor.set_drive(
+                        data["motor"], data["dir"], data["speed"])
+            except:
+                stop_all_motors()
+                clientsocket.close()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except (KeyboardInterrupt, SystemExit) as e:
+        myMotor.disable()
+        sys.exit(0)
